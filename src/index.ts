11a9fc525c2733cf3ceffe047b13116338af2b18
CarlosTolenk <carlos.tolentinoe@gmail.com>
2023-07-29 09:01:33 -0400

    Apply Middlewares Basic for Express

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import compression from 'compression';
+import cors from 'cors';
+
 import { rootHandler, helloHandler } from './handlers';
 
 const app = express();
 const port = process.env.PORT || '3000';
+
+app.use(helmet())
+    .use(cors())
+    .use(bodyParser.json())
+    .use(
+        bodyParser.urlencoded({
+            extended: false
+        })
+    )
+    .use(compression());
+
 
 app.get('/', rootHandler);
 app.get('/hello/:name', helloHandler);
